1a3d18afe1430c4d710e360feee74316d5335bba
Nemesty <frederic.duval@minhbo.fr>
2025-01-02 18:04:35 +0100

    Take input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Devine le nombre !");
+
+    println!("Veuillez entrer un nombre :");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Échec de lecture de la ligne.");
+
+    println!("Vous avez deviné : {}", guess);
 }
